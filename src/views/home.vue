aef2bd097a030f29b71e03af4fdd4a6d4c5cc187
Jason011125 <zic022@ucsd.edu>
2024-07-03 13:34:38 +0800

    增加了主页的主体以及主页header

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,5 +1,15 @@
 <template>
-  <h1>home</h1>
+  <div class="home-header">
+<!--    todo: 需要让home-header粘滞在顶部-->
+    <h1 color="black">header</h1>
+  </div>
+  <div class="home-container">
+    <div class="title-container">
+      <h1>智慧土豆</h1>
+      <h2>机器人技术支持专家</h2>
+    </div>
+  </div>
+
 </template>
 
 <script setup>
@@ -12,6 +22,29 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+.home-container{
+  background-image: url("../assets/home-bg.png");
+  background-size: cover;
+  width: 100%;
+  height: 493px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.title-container{
+  color: #FFFFFF;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.home-header{
+  color: black;
+  position: sticky;
+  top: 0;
+  background-color: white; /* 可以根据需要设置背景颜色 */
+  z-index: 1000; /* 确保 header 处于顶层 */
+}
 
 </style>
