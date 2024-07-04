797ec5f923824e7f94e46eaf1f1adca79b59a67e
(Messi) Junlin Wu <tallmessiwu@qq.com>
2024-07-04 12:17:13 +0800

    完成了页眉

--- src/components/the-header.vue
+++ src/components/the-header.vue
@@ -1,16 +1,41 @@
 <template>
   <el-container>
-    <el-header>
-      <el-row justify="space-between" align="middle">
+    <el-header class="common">
+      <el-row align="middle">
 
         <!--logo-->
-        <el-col :span="16" :offset="2">
+        <el-col :span="6">
           <el-row align="middle">
             <el-image class="logo" :src="getAssetsFile('text-logo.png')" fit="contain"></el-image>
           </el-row>
         </el-col>
 
+        <!--标签-->
+        <el-col :span="17" :offset="1">
+          <el-row justify="end" align="middle">
+            <el-col :span="4">
+              <el-link>首页</el-link>
+            </el-col>
+            <el-col :span="4">
+              <el-link>开源资料</el-link>
+            </el-col>
+            <el-col :span="4">
+              <el-link>硬件产品</el-link>
+            </el-col>
+            <el-col :span="4">
+              <el-link>竞赛解决方案</el-link>
+            </el-col>
+            <el-col :span="4">
+              <el-link>STEAM教育</el-link>
+            </el-col>
+            <el-col :span="4">
+              <el-link>联系我们</el-link>
+            </el-col>
+          </el-row>
+        </el-col>
+
       </el-row>
+
     </el-header>
   </el-container>
 </template>
@@ -26,5 +51,38 @@ export default {
 </script>
 
 <style scoped>
+.common {
+  background-color: #31354D;
+  height: 75px;
+}
+
+.el-row {
+  height: 75px;
+}
+
+
+.logo {
+  height: 35px;
+}
+
+.el-link {
+  font-family: "Microsoft YaHei", serif;
+  color: white;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 32px;
+  letter-spacing: 0.1em;
+  text-align: left;
+
+  &:hover {
+    color: #8596CB;
+
+    &::after {
+      content: "";
+      border-bottom: 1px solid white;
+    }
+  }
+}
+
 
 </style>
