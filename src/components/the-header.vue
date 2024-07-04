9085acd39c042c78d92d9cf732d1ac45149b62e8
(Messi) Junlin Wu <tallmessiwu@qq.com>
2024-07-04 11:00:50 +0800

    获取了图片并创建了element plus使用assets的函数

--- src/components/the-header.vue
+++ src/components/the-header.vue
@@ -1,9 +1,22 @@
 <template>
-  <h1>the-header</h1>
+  <el-container>
+    <el-header>
+      <el-row justify="space-between" align="middle">
+
+        <!--logo-->
+        <el-col :span="16" :offset="2">
+          <el-row align="middle">
+            <el-image class="logo" :src="getAssetsFile('logo-with-text.png')" fit="contain"></el-image>
+          </el-row>
+        </el-col>
+
+      </el-row>
+    </el-header>
+  </el-container>
 </template>
 
 <script setup>
-
+import {getAssetsFile} from "../utils/getAssetsFile";
 </script>
 
 <script>
