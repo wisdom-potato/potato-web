f81b489a65c17274012554b77e55af588d0e1235
(Messi) Junlin Wu <tallmessiwu@qq.com>
2024-07-06 19:31:54 +0800

    完成了硬件产品页面

--- src/views/hardware-products.vue
+++ src/views/hardware-products.vue
@@ -1,4 +1,6 @@
 <template>
+
+  <!--硬件产品专区板块-->
   <el-row class="mid-title">硬件产品专区</el-row>
   <el-row align="middle" justify="center" class="slider-container">
     <img src="../assets/inactive-slider-switch-triangle.png" alt="" class="header-switch-triangle">
@@ -11,6 +13,7 @@
     <img src="../assets/active-slider-switch-triangle.png" alt="" class="header-switch-triangle">
   </el-row>
 
+  <!--现有产品板块-->
   <el-row class="product-title">现有产品</el-row>
   <el-row class="product-store-link" justify="end">前往官方店铺查看更多</el-row>
 
@@ -19,22 +22,47 @@
 
       <div class="product-card" id="product-card-1">
         <div class="product-text">土豆泥<br/>系列</div>
+        <!--产品图片-->
         <img id="product-1" src="../assets/product-1.png" alt=""/>
       </div>
 
       <div class="product-card" id="product-card-2">
         <div class="product-text">比赛<br/>定制</div>
+        <!--产品图片-->
         <img id="product-2" src="../assets/product-2.png" alt=""/>
       </div>
 
       <div class="product-card" id="product-card-3">
         <div class="product-text">敬请<br/>期待</div>
+        <!--产品图片-->
         <img id="product-3" src="../assets/product-3.png" alt=""/>
       </div>
+
     </el-row>
   </el-row>
 
+
+  <!--分割线-->
   <el-divider/>
+
+  <!--流程介绍板块-->
+  <el-row class="roadmap-title" justify="center">专业设计团队 一站式解决硬件需求</el-row>
+  <!--流程-->
+  <el-row justify="center">
+    <el-row class="steps-container" justify="space-between">
+      <div class="step">确定需求</div>
+      <div class="step">设计打样</div>
+      <div class="step">批量生产</div>
+    </el-row>
+  </el-row>
+  <!--流程配图-->
+  <el-row justify="center">
+    <el-row class="step-images-container" justify="space-between">
+      <img class="step-images" src="../assets/step-image-1.png" alt=""/>
+      <img class="step-images" src="../assets/step-image-2.png" alt=""/>
+      <img class="step-images" src="../assets/step-image-3.png" alt=""/>
+    </el-row>
+  </el-row>
 
 </template>
 
@@ -206,6 +234,58 @@ export default {
   position: absolute;
   top: 5%;
   left: 5%;
+}
+
+.roadmap-title {
+  font-family: Microsoft YaHei, serif;
+  font-size: 35px;
+  font-weight: 700;
+  line-height: 46.19px;
+  letter-spacing: 0.12em;
+  text-align: left;
+
+  color: #31354D;
+
+  margin-top: 80px;
+  margin-bottom: 56px;
+}
+
+.steps-container {
+  width: 938px;
+  height: 104px;
+
+  margin-bottom: 50px;
+}
+
+.step {
+  width: 286px;
+  height: 104px;
+  border-radius: 20px;
+  background-color: #EFF1F8;
+
+  //styleName: 中标题;
+  font-family: Microsoft YaHei, serif;
+  font-size: 35px;
+  font-weight: 700;
+  letter-spacing: 0.12em;
+  text-align: center;
+  line-height: 104px;
+  color: #6374B6;
+}
+
+.step-images-container{
+  width: 1060px;
+  height: 316px;
+
+  margin-bottom: 220px;
+}
+
+.step-images{
+  width: 316px;
+  height: 316px;
+  border-radius: 10px;
+
+  object-fit: cover;
 }
 
 </style>
